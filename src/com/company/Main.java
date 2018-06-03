4210af0a67e8e43cb583869eb6f4521d96932073
NataliMo <moshkova-n1@yandex.ru>
2018-06-03 21:58:26 +0300

    выполнение заданий на выбор калькулятор и массив

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,19 @@
 package com.company;
 
+import java.io.IOException;
 import java.util.Scanner;
+import java.lang.*;
 /** @autor Natalya Moshkova */
 public class Main {
 
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        float a,b,c;
+        System.out.println("Выберите нужную задачу: 1 - калькулятор, 2 - строковый массив");
+        byte w;
+        w = scan.nextByte();
+
+        if(w == 1) {
+            float a,b,c;
         String operator;
         System.out.println("Введите первое число");
         a = scan.nextFloat();
@@ -35,7 +40,28 @@ public class Main {
         }
 
 
-        scan.close();
+        }
+        else {
+
+            System.out.println("Введите длинну строкового массива");
+            int n = scan.nextInt();
+            System.out.println("введите через enter слова");
+            String[] arrayString = new String[n + 1];
+            for (int i = 0; i < arrayString.length; i++) {
+                arrayString[i] = scan.nextLine();
+
+            }
+
+            int max = 1;
+            for (int j = 0; j < arrayString.length; j++) {
+                if (arrayString[j].length() > max) {
+                    max = arrayString[j].length();
+                }
+            }
+            System.out.println("Максимальный элемент массива: " + max);
+            scan.close();
+        }
+
 
     }
 }
