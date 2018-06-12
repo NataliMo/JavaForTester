a87a00d6a22e5799e85a7470230c6ad81f528450
NataliMo <moshkova-n1@yandex.ru>
2018-06-12 22:41:32 +0300

    #

---
+++ src/com/company/present/sweet/Candies.java
@@ -0,0 +1,12 @@
+package com.company.present.sweet;
+
+/**
+ * Created by natalamoskova on 08.06.2018.
+ */
+public class Candies extends Testy {
+
+    public Candies(String brand, double price, double weight, int id) {
+        super(brand, price, weight, id);
+    }
+
+}
